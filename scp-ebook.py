4642c00807a3fe3d2198b40a035d96e0e739fe26
anqxyr <anqxyr@gmail.com>
2014-06-26 18:13:41 +0300

    + prettify(), make_epub()

--- scp-ebook.py
+++ scp-ebook.py
@@ -4,43 +4,52 @@ from ebooklib import epub
 from urllib import urlopen
 from bs4 import BeautifulSoup
 
+
 def get_page(url):
+    '''Scrape the contents of the given url.'''
     print "downloading " + url
     page = BeautifulSoup(urlopen(url).read())
     title = str(page.select("#page-title")[0].text).strip()
     content = page.select("#page-content")[0]
     content.div.decompose()
-    return [title,content]
+    return {"title": str(title), "content": str(content)}
+
+
+def make_epub(title, pages):
+    book = epub.EpubBook()
+    spine = []
+    book.set_title(title)
+    for p in pages:
+        epub_page = epub.EpubHtml(title = p["title"], file_name = p["title"].lower().replace(" ", "_") + ".xhtml")
+        epub_page.content = p["content"]
+        book.add_item(epub_page)
+        spine.append(epub_page)
+    #book.toc = (p in spine) #???
+    book.add_item(epub.EpubNcx())
+    book.add_item(epub.EpubNav())
+    book.spine = spine
+    return book
+
+def prettify(title, content):
+    chapter = "<h2>" + title + "</h2>" + content
+    return chapter
 
 
 def main():
-    url_list=["http://www.scp-wiki.net/scp-1511",
+    url_list = ["http://www.scp-wiki.net/scp-1511",
         "http://www.scp-wiki.net/scp-1425",
         "http://www.scp-wiki.net/scp-9005-2",
         "http://www.scp-wiki.net/quiet-days",
         "http://www.scp-wiki.net/black-white-black-white-black-white-black-white-black-white"]
-    book = epub.EpubBook()
-    spine = ["nav"]
-    book.set_title("SCP Ebook test")
-    book.set_language("en")
-    cc_intro = epub.EpubHtml(title="", file_name="license.xhtml")
-    cc_intro.content = "Placeholder Creative Commons disclaimer."
-    book.add_item(cc_intro)
-    spine.append(cc_intro)
-    ch_n = 1
+    pages = []
+    pages.append({"title": "Title Page", "content": "Placeholder Title Page"})
+    pages.append({"title": "License", "content": "Placeholder Creative Commons disclaimer"})
     for url in url_list:
         page = get_page(url)
-        new_chapter = epub.EpubHtml(title=page[0], file_name=page[0].lower().replace(" ", "_") + ".xhtml")
-        new_chapter.content = str(page[1])
-        book.add_item(new_chapter)
-        spine.append(new_chapter)
-    attrib = epub.EpubHtml(title="Attributions", file_name="attributions.xhtml")
-    attrib.content = "Placeholder; list of article authors, image artists, etc, etc."
-    book.add_item(attrib)
-    spine.append(attrib)
-    book.add_item(epub.EpubNcx())
-    book.add_item(epub.EpubNav())
-    book.spine = spine
+        page["content"] = prettify(page["title"], page["content"])
+        pages.append(page)
+    pages.append({"title": "Appendix", "content": "Placeholder; list of article authors, image artists, etc, etc."})
+    book = make_epub("SCP TEST Ebook", pages)
     epub.write_epub("test.epub", book, {})
 
 main()
