db5d4e8c0c81cb26edb1219c27d6d34f0425f181
anqxyr <anqxyr@gmail.com>
2014-11-09 22:52:15 +0300

    authentication and page lock

--- wikidot.py
+++ wikidot.py
@@ -24,31 +24,40 @@ req = get_req_session()
 
 def module(name, pageid, **kwargs):
     """Retrieve data from the specified wikidot module."""
-    headers = {'Content-Type': 'application/x-www-form-urlencoded;',
-               'Cookie': 'wikidot_token7=123456;'}
+    headers = {'Content-Type': 'application/x-www-form-urlencoded;'}
     payload = {'page_id': pageid,
                'pageId': pageid,  # fuck wikidot
                'moduleName': name,
                'wikidot_token7': '123456'}
+    cookies = {'wikidot_token7': '123456'}
+    for i in req.cookies:
+        cookies[i.name] = i.value
     for k, v in kwargs.items():
         payload[k] = v
     data = req.post('http://testwiki2.wikidot.com/ajax-module-connector.php',
-                    data=payload, headers=headers)
+                    data=payload, headers=headers, cookies=cookies)
     return data.json()
 
 
-def auth():
-    auth_url = 'http://urllogin.wikidot.com/login/code/2#{}:{}'
-    user = 'anqxyr'
-    pasw = '2A![]M/r}%t?,"GWQ.eH#uaukC3}#.*#uv=yd23NvkpuLgN:kPOBARb}:^IDT?%j'
-    resp = req.get(auth_url.format(user, pasw))
-    print(resp)
+def auth(username, password):
+    payload = {'login': username,
+               'password': password,
+               'originSiteId': '699677',
+               'action': 'Login2Action',
+               'event': 'login'}
+    url = 'https://www.wikidot.com/default--flow/login__LoginPopupScreen'
+    req.post(url, data=payload)
 
 
 def edit_page(url, content):
     page_id = '24075979'
-    lock = module('edit/PageEditModule', page_id)
-    print(lock)
+    params = {'mode': 'page',
+              'wiki_page': 'page1'}
+    lock = module('edit/PageEditModule', page_id, **params)
+    import pprint
+    pprint.pprint(lock)
+    print(lock['lock_id'])
+    print(lock['lock_secret'])
     exit()
     payload = {'source': 'it works!',
                'comments': 'yay'}
@@ -59,7 +68,9 @@ def edit_page(url, content):
 
 
 def main():
-    auth()
+    pasw = '2A![]M/r}%t?,"GWQ.eH#uaukC3}#.*#uv=yd23NvkpuLgN:kPOBARb}:^IDT?%j'
+    auth(username='anqxyr',
+         password=pasw)
     edit_page(None, None)
 
 
