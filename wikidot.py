28c30076ece26c5e59ce0abb5973af54855c3ecf
anqxyr <anqxyr@gmail.com>
2014-11-10 20:34:25 +0300

    modified into WikidotConnector

--- wikidot.py
+++ wikidot.py
@@ -6,79 +6,71 @@
 
 import requests
 
-###############################################################################
-# Global connection session
-###############################################################################
-
-
-def get_req_session():
-    req = requests.Session()
-    req.mount('http://testwiki2.wikidot.com',
-              requests.adapters.HTTPAdapter(max_retries=5))
-    return req
-
-req = get_req_session()
+from collections import namedtuple
 
 ###############################################################################
 
 
-def module(name, pageid, **kwargs):
-    """Retrieve data from the specified wikidot module."""
-    headers = {'Content-Type': 'application/x-www-form-urlencoded;'}
-    payload = {'page_id': pageid,
-               'pageId': pageid,  # fuck wikidot
-               'moduleName': name,
-               'wikidot_token7': '123456'}
-    cookies = {'wikidot_token7': '123456'}
-    for i in req.cookies:
-        cookies[i.name] = i.value
-    for k, v in kwargs.items():
-        payload[k] = v
-    data = req.post('http://testwiki2.wikidot.com/ajax-module-connector.php',
-                    data=payload, headers=headers, cookies=cookies)
-    return data.json()
+class WikidotConnector:
 
+    def __init__(self, site):
+        if site[-1] != '/':
+            site += '/'
+        self.site = site
+        req = requests.Session()
+        req.mount(site, requests.adapters.HTTPAdapter(max_retries=5))
+        self.req = req
 
-def auth(username, password):
-    payload = {'login': username,
-               'password': password,
-               'action': 'Login2Action',
-               'event': 'login'}
-    url = 'https://www.wikidot.com/default--flow/login__LoginPopupScreen'
-    req.post(url, data=payload)
+    def module(self, name, pageid, **kwargs):
+        """Retrieve data from the specified wikidot module."""
+        headers = {'Content-Type': 'application/x-www-form-urlencoded;'}
+        payload = {
+            'page_id': pageid,
+            'pageId': pageid,  # fuck wikidot
+            'moduleName': name,
+            'wikidot_token7': '123456'}
+        cookies = {'wikidot_token7': '123456'}
+        for i in self.req.cookies:
+            cookies[i.name] = i.value
+        for k, v in kwargs.items():
+            payload[k] = v
+        data = self.req.post(self.site + 'ajax-module-connector.php',
+                             data=payload, headers=headers, cookies=cookies)
+        return data.json()
 
+    def auth(self, username, password):
+        payload = {
+            'login': username,
+            'password': password,
+            'action': 'Login2Action',
+            'event': 'login'}
+        url = 'https://www.wikidot.com/default--flow/login__LoginPopupScreen'
+        self.req.post(url, data=payload)
 
-def get_meta(url):
-    # TODO: actually write the method; add sql cache
-    return '24075979'
-
-
-def edit_page(url, content, comments=None, title=None):
-    page_id, old_title = get_meta(url)
-    if title is None:
-        title = old_title
-    lock = module('edit/PageEditModule', page_id, mode='page')
-    params = {'source': content,
-              'comments': comments,
-              'title': title,
-              'lock_id': lock['lock_id'],
-              'lock_secret': lock['lock_secret'],
-              'revision_id': lock['page_revision_id'],
-              'action': 'WikiPageAction',
-              'event': 'savePage',
-              'wiki_page': url.split('/')[-1]}
-    module('Empty', page_id, **params)
+    def edit_page(self, page_id, wiki_page, data):
+        lock = self.module('edit/PageEditModule', page_id, mode='page')
+        params = {
+            'source': data.source,
+            'comments': data.comments,
+            'title': data.title,
+            'lock_id': lock['lock_id'],
+            'lock_secret': lock['lock_secret'],
+            'revision_id': lock['page_revision_id'],
+            'action': 'WikiPageAction',
+            'event': 'savePage',
+            'wiki_page': wiki_page}
+        self.module('Empty', page_id, **params)
 
 ###############################################################################
 
 
 def main():
+    wiki = WikidotConnector('http://testwiki2.wikidot.com')
     pasw = '2A![]M/r}%t?,"GWQ.eH#uaukC3}#.*#uv=yd23NvkpuLgN:kPOBARb}:^IDT?%j'
-    auth(username='anqxyr',
-         password=pasw)
-    #x = module('viewsource/ViewSourceModule', '24075979')
-    #print(x)
-    edit_page(None, None)
+    wiki.auth(username='anqxyr', password=pasw)
+    edit = namedtuple('PageEditData', 'title source comments')
+    e = edit('I am the Title, █████', 'no source here', None)
+    wiki.edit_page('24075979', 'page1', e)
 
 
 if __name__ == "__main__":
