5fd75174000a92c73b9bff7cee8141eae7ee8a0f
anqxyr <anqxyr@gmail.com>
2015-01-09 23:45:12 +0300

    add print_basic to stats.py

--- stats.py
+++ stats.py
@@ -7,6 +7,7 @@
 #import csv
 #import matplotlib
 #import re
+import collections
 import crawler
 import logging
 import numpy as np
@@ -28,152 +29,160 @@ logger.setLevel(logging.DEBUG)
 class StatGenerator:
 
     def __init__(self):
-        self.basic = defaultdict(int)
-        self.basic_lists = defaultdict(list)
-        self.user = defaultdict(lambda: defaultdict(int))
-        self.author = defaultdict(lambda: defaultdict(int))
-        self.user_tags = defaultdict(lambda: defaultdict(int))
-        self.author_tags = defaultdict(lambda: defaultdict(int))
-        self.year_votes = defaultdict(
-            lambda: defaultdict(lambda: defaultdict(int)))
+        pass
 
     ###########################################################################
     # Page Processors
     ###########################################################################
 
-    def proc_basic(self, page, suffix=''):
-        self.basic['page count' + suffix] += 1
-        if page.rating is not None:
-            self.basic_lists['ratings' + suffix].append(page.rating)
-        self.basic['wordcounts' + suffix] += page.wordcount
-        self.basic['revisions' + suffix] += len(page.history)
-        self.basic['comments' + suffix] += len(page.comments)
-        if (page.author not in self.basic_lists['unique authors' + suffix]
-            and not page.author.startswith('Anonymous')
-                and not page.author == '(account deleted)'):
-            self.basic_lists['unique authors' + suffix].append(page.author)
-        for i in page.votes:
-            if i.user not in self.basic_lists['unique voters' + suffix]:
-                self.basic_lists['unique voters' + suffix].append(i.user)
-            if i.value == 1:
-                self.basic['upvotes' + suffix] += 1
-            else:
-                self.basic['downvotes' + suffix] += 1
-        if not suffix and 'scp' in page.tags:
-            self.proc_basic(page, suffix=': skips')
-        if not suffix and 'tale' in page.tags:
-            self.proc_basic(page, suffix=': tales')
-
-    def proc_user(self, page):
-        for i in page.history:
-            self.user[i.user]['revisions'] += 1
-        for i in page.votes:
-            if i.value == 1:
-                self.user[i.user]['upvotes'] += 1
-            else:
-                self.user[i.user]['downvotes'] += 1
-
-    def proc_author(self, page):
-        self.author[page.author]['pages created'] += 1
-        if page.rating is not None:
-            self.author[page.author]['net rating'] += page.rating
-        self.author[page.author]['wordcount'] += page.wordcount
-        self.author[page.author]['image count'] += len(page.images)
-
-    def proc_user_tags(self, page):
-        for v in page.votes:
-            for t in page.tags:
-                self.user_tags[v.user][t] += v.value
-
-    def proc_author_tags(self, page):
-        for t in page.tags:
-            self.author_tags[page.author][t] += 1
-
-    def proc_year_votes(self, page):
-        for v in page.votes:
-            if v.value == 1:
-                self.year_votes[v.user][
-                    page.creation_time.year]['upvotes'] += 1
-            else:
-                self.year_votes[v.user][
-                    page.creation_time.year]['downvotes'] += 1
-
     ###########################################################################
     # Generator
     ###########################################################################
 
+    def _gen_pageframe(self):
+        col_extractors = {
+            'url': lambda x: x.url,
+            'title': lambda x: x.title,
+            'rating': lambda x: x.rating,
+            'upvotes': lambda x: len([v for v in x.votes if v.value == 1]),
+            'downvotes': lambda x: len([v for v in x.votes if v.value == -1]),
+            'author': lambda x: x.author,
+            'scp': lambda x: 'scp' in x.tags,
+            'tale': lambda x: 'tale' in x.tags,
+            'wordcount': lambda x: x.wordcount,
+            'comments': lambda x: len(x.comments),
+            'revisions': lambda x: len(x.history),
+            'images': lambda x: len(x.images),
+        }
+        columns = collections.defaultdict(dict)
+        for n, url in enumerate(crawler.Page.sn.list_all_pages()):
+            logger.info('Processing page #{}'.format(n))
+            p = crawler.Page(url)
+            for label in col_extractors:
+                func = col_extractors[label]
+                columns[label][p._pageid] = func(p)
+        pageframe = pd.DataFrame(columns)
+        return pageframe
+
     def generate(self):
-        with crawler.Page.from_snapshot() as sn:
-            for url in sn.list_all_pages():
-                logger.info('Processing page: {}'.format(url))
-                p = crawler.Page(url)
-                self.proc_basic(p)
-                self.proc_user(p)
-                self.proc_author(p)
-                self.proc_user_tags(p)
-                self.proc_author_tags(p)
-                self.proc_year_votes(p)
+        with crawler.Page.from_snapshot():
+            logger.info('Generating page list')
+            pageframe = self._gen_pageframe()
+            pageframe.to_csv('pageframe.csv', index_label='index')
+            self.pageframe = pageframe
+
+    def load(self):
+        self.pageframe = pd.read_csv('pageframe.csv', index_col='index')
 
     ###########################################################################
     # Output Methods
     ###########################################################################
 
     def print_basic(self):
-        pages = 'Pages on the site: {} [{}/{}]'
-        pages = pages.format(
-            self.basic['page count'],
-            self.basic['page count: skips'],
-            self.basic['page count: tales'])
-        print(pages)
-        authors = 'Unique authors: {} [{}/{}]'
-        authors = authors.format(
-            len(self.basic_lists['unique authors']),
-            len(self.basic_lists['unique authors: skips']),
-            len(self.basic_lists['unique authors: tales']))
-        print(authors)
-        voters = 'Unique voters: {} [{}/{}]'
-        voters = voters.format(
-            len(self.basic_lists['unique voters']),
-            len(self.basic_lists['unique voters: skips']),
-            len(self.basic_lists['unique voters: tales']))
-        print(voters)
-        votes = ('Total votes (upvotes/downvotes):'
-                 ' {} ({}/{}) [{} ({}/{}) / {} ({}/{})]')
-        votes = votes.format(
-            self.basic['upvotes'] + self.basic['downvotes'],
-            self.basic['upvotes'],
-            self.basic['downvotes'],
-            self.basic['upvotes: skips'] + self.basic['downvotes: skips'],
-            self.basic['upvotes: skips'],
-            self.basic['downvotes: skips'],
-            self.basic['upvotes: tales'] + self.basic['downvotes: tales'],
-            self.basic['upvotes: tales'],
-            self.basic['downvotes: tales'])
-        print(votes)
-        ratings = ('Total rating (average/mode):'
-                   ' {} ({:.0f}/{}) [{} ({:.0f}/{}) / {} ({:.0f}/{})]')
-        ratings = ratings.format(
-            sum(self.basic_lists['ratings']),
-            statistics.mean(self.basic_lists['ratings']),
-            statistics.mode(self.basic_lists['ratings']),
-            sum(self.basic_lists['ratings: skips']),
-            statistics.mean(self.basic_lists['ratings: skips']),
-            statistics.mode(self.basic_lists['ratings: skips']),
-            sum(self.basic_lists['ratings: tales']),
-            statistics.mean(self.basic_lists['ratings: tales']),
-            statistics.mode(self.basic_lists['ratings: tales']))
-        print(ratings)
+        pf = self.pageframe
+        template_text = '| {:<33} | {:<8} | {:<8} | {:<8} |'
+        template_float = '| {:<33} | {:<8.0f} | {:<8.0f} | {:<8.0f} |'
+        print('-' * 70)
+        print(template_text.format(
+            'Field Name',
+            'total',
+            'skips',
+            'tales'))
+        print('-' * 70)
+        print(template_float.format(
+            'Number of pages on the site:',
+            pf.index.size,
+            pf[pf.scp].index.size,
+            pf[pf.tale].index.size))
+        print(template_float.format(
+            'Number of unique authors:',
+            pf.author.nunique(),
+            pf[pf.scp].author.nunique(),
+            pf[pf.tale].author.nunique()))
+        print(template_float.format(
+            'Total number of votes:',
+            pf.upvotes.sum() + pf.downvotes.sum(),
+            pf[pf.scp].upvotes.sum() + pf[pf.scp].downvotes.sum(),
+            pf[pf.tale].upvotes.sum() + pf[pf.tale].downvotes.sum()))
+        print(template_float.format(
+            '    Upvotes:',
+            pf.upvotes.sum(),
+            pf[pf.scp].upvotes.sum(),
+            pf[pf.tale].upvotes.sum()))
+        print(template_float.format(
+            '    Downvotes:',
+            pf.downvotes.sum(),
+            pf[pf.scp].downvotes.sum(),
+            pf[pf.tale].downvotes.sum()))
+        print('-' * 70)
+        print(template_float.format(
+            'Total Net Rating:',
+            pf.rating.sum(),
+            pf[pf.scp].rating.sum(),
+            pf[pf.tale].rating.sum()))
+        print(template_float.format(
+            '    Average:',
+            pf.rating.mean(),
+            pf[pf.scp].rating.mean(),
+            pf[pf.tale].rating.mean()))
+        print(template_float.format(
+            '    Mode:',
+            pf.rating.mode()[0],
+            pf[pf.scp].rating.mode()[0],
+            pf[pf.tale].rating.mode()[0]))
+        print(template_float.format(
+            '    Deviation:',
+            pf.rating.std(),
+            pf[pf.scp].rating.std(),
+            pf[pf.tale].rating.std()))
+        print('-' * 70)
+        print(template_float.format(
+            'Total Wordcount:',
+            pf.wordcount.sum(),
+            pf[pf.scp].wordcount.sum(),
+            pf[pf.tale].wordcount.sum()))
+        print(template_float.format(
+            '    Average:',
+            pf.wordcount.mean(),
+            pf[pf.scp].wordcount.mean(),
+            pf[pf.tale].wordcount.mean()))
+        print(template_float.format(
+            'Total Comments:',
+            pf.comments.sum(),
+            pf[pf.scp].comments.sum(),
+            pf[pf.tale].comments.sum()))
+        print(template_float.format(
+            '    Average:',
+            pf.comments.mean(),
+            pf[pf.scp].comments.mean(),
+            pf[pf.tale].comments.mean()))
+        print(template_float.format(
+            'Total Revisions:',
+            pf.revisions.sum(),
+            pf[pf.scp].revisions.sum(),
+            pf[pf.tale].revisions.sum()))
+        print(template_float.format(
+            '    Average:',
+            pf.revisions.mean(),
+            pf[pf.scp].revisions.mean(),
+            pf[pf.tale].revisions.mean()))
+        print(template_float.format(
+            'Total Images:',
+            pf.images.sum(),
+            pf[pf.scp].images.sum(),
+            pf[pf.tale].images.sum()))
+        print('-' * 70)
 
-###############################################################################
 
 ###############################################################################
 
 
 def main():
     gen = StatGenerator()
-    gen.generate()
-    #print(gen.basic_lists['unique authors'])
+    gen.load()
     gen.print_basic()
+    #print(gen.basic_lists['unique authors'])
+    #gen.print_basic()
     #print(gen.basic_lists['ratings'])
     pass
 
