866dd852f260b9b8b7ac635702e31a56b935481e
anqxyr <anqxyr@gmail.com>
2015-01-13 13:35:48 +0300

    improve DataFrame generation and handling

--- stats.py
+++ stats.py
@@ -7,10 +7,13 @@
 #import csv
 #import matplotlib
 #import re
+import arrow
+import bs4
 import collections
 import crawler
 import logging
-import numpy as np
+import re
+#import numpy as np
 import pandas as pd
 
 
@@ -29,7 +32,7 @@ logger.setLevel(logging.DEBUG)
 class StatGenerator:
 
     def __init__(self):
-        pass
+        self.datadir = crawler.Snapshot.database_directory + 'stats/'
 
     ###########################################################################
     # Page Processors
@@ -39,140 +42,150 @@ class StatGenerator:
     # Generator
     ###########################################################################
 
-    def _gen_pageframe(self):
-        col_extractors = {
-            'url': lambda x: x.url,
-            'title': lambda x: x.title,
-            'rating': lambda x: x.rating,
-            'upvotes': lambda x: len([v for v in x.votes if v.value == 1]),
-            'downvotes': lambda x: len([v for v in x.votes if v.value == -1]),
-            'author': lambda x: x.author,
-            'scp': lambda x: 'scp' in x.tags,
-            'tale': lambda x: 'tale' in x.tags,
-            'wordcount': lambda x: x.wordcount,
-            'comments': lambda x: len(x.comments),
-            'revisions': lambda x: len(x.history),
-            'images': lambda x: len(x.images),
-        }
-        columns = collections.defaultdict(dict)
-        for n, url in enumerate(crawler.Page.sn.list_all_pages()):
-            logger.info('Processing page #{}'.format(n))
-            p = crawler.Page(url)
-            for label in col_extractors:
-                func = col_extractors[label]
-                columns[label][p._pageid] = func(p)
-        pageframe = pd.DataFrame(columns)
-        return pageframe
+    def _generate_pages(self, frames, page):
+        basic = ('url', 'title', 'rating', 'author', 'created', 'wordcount')
+        for x in basic:
+            frames['pages'][x][page._pageid] = getattr(page, x)
+        lists = ('images', 'comments', 'history')
+        for x in lists:
+            frames['pages'][x][page._pageid] = len(getattr(page, x))
+        for i in page.tags:
+            index = len(frames['tags']['page'])
+            frames['tags']['page'][index] = page._pageid
+            frames['tags']['tag'][index] = i
+
+    def _generate_votes(self, frames, page):
+        for i in page.votes:
+            index = len(frames['votes']['page'])
+            for x in ('user', 'value'):
+                frames['votes'][x][index] = getattr(i, x)
+            frames['votes']['page'][index] = page._pageid
+
+    def _generate_revisions(self, frames, page):
+        for i in page.history:
+            index = len(frames['revisions']['page'])
+            for x in ('user', 'time'):
+                frames['revisions'][x][index] = getattr(i, x)
+            frames['revisions']['page'][index] = page._pageid
+
+    def _generate_comments(self, frames, page):
+        for i in page.comments:
+            for x in ('user', 'time', 'title'):
+                frames['comments'][x][i.post_id] = getattr(i, x)
+            wc = len(re.findall(r"[\w'█_-]+",
+                                bs4.BeautifulSoup(i.content).text))
+            frames['comments']['wordcount'][i.post_id] = wc
+            frames['comments']['page'][i.post_id] = page._pageid
 
     def generate(self):
+        frames = collections.defaultdict(lambda: collections.defaultdict(dict))
         with crawler.Page.from_snapshot():
-            logger.info('Generating page list')
-            pageframe = self._gen_pageframe()
-            pageframe.to_csv('pageframe.csv', index_label='index')
-            self.pageframe = pageframe
+            logger.info('Generating DataFrame objects')
+            for n, url in enumerate(crawler.Page.sn.list_all_pages()):
+                logger.info('Processing page #{}'.format(n))
+                p = crawler.Page(url)
+                self._generate_pages(frames, p)
+                self._generate_votes(frames, p)
+                self._generate_revisions(frames, p)
+                self._generate_comments(frames, p)
+        for i in frames:
+            frame = pd.DataFrame(frames[i])
+            frame.to_csv(
+                '{}_{}.csv'.format(self.datadir, i),
+                index_label='index')
+            setattr(self, i, frame)
 
     def load(self):
-        self.pageframe = pd.read_csv('pageframe.csv', index_col='index')
+        for i in ('pages', 'votes', 'tags', 'revisions', 'comments'):
+            frame = pd.read_csv(
+                '{}_{}.csv'.format(self.datadir, i),
+                index_col='index')
+            setattr(self, i, frame)
 
     ###########################################################################
     # Output Methods
     ###########################################################################
 
     def print_basic(self):
-        pf = self.pageframe
-        template_text = '| {:<33} | {:<8} | {:<8} | {:<8} |'
-        template_float = '| {:<33} | {:<8.0f} | {:<8.0f} | {:<8.0f} |'
-        print('-' * 70)
-        print(template_text.format(
-            'Field Name',
-            'total',
-            'skips',
-            'tales'))
-        print('-' * 70)
-        print(template_float.format(
+        msgs = (
             'Number of pages on the site:',
-            pf.index.size,
-            pf[pf.scp].index.size,
-            pf[pf.tale].index.size))
-        print(template_float.format(
             'Number of unique authors:',
-            pf.author.nunique(),
-            pf[pf.scp].author.nunique(),
-            pf[pf.tale].author.nunique()))
-        print(template_float.format(
             'Total number of votes:',
-            pf.upvotes.sum() + pf.downvotes.sum(),
-            pf[pf.scp].upvotes.sum() + pf[pf.scp].downvotes.sum(),
-            pf[pf.tale].upvotes.sum() + pf[pf.tale].downvotes.sum()))
-        print(template_float.format(
             '    Upvotes:',
-            pf.upvotes.sum(),
-            pf[pf.scp].upvotes.sum(),
-            pf[pf.tale].upvotes.sum()))
-        print(template_float.format(
             '    Downvotes:',
-            pf.downvotes.sum(),
-            pf[pf.scp].downvotes.sum(),
-            pf[pf.tale].downvotes.sum()))
-        print('-' * 70)
-        print(template_float.format(
             'Total Net Rating:',
-            pf.rating.sum(),
-            pf[pf.scp].rating.sum(),
-            pf[pf.tale].rating.sum()))
-        print(template_float.format(
             '    Average:',
-            pf.rating.mean(),
-            pf[pf.scp].rating.mean(),
-            pf[pf.tale].rating.mean()))
-        print(template_float.format(
             '    Mode:',
-            pf.rating.mode()[0],
-            pf[pf.scp].rating.mode()[0],
-            pf[pf.tale].rating.mode()[0]))
-        print(template_float.format(
             '    Deviation:',
-            pf.rating.std(),
-            pf[pf.scp].rating.std(),
-            pf[pf.tale].rating.std()))
-        print('-' * 70)
-        print(template_float.format(
             'Total Wordcount:',
-            pf.wordcount.sum(),
-            pf[pf.scp].wordcount.sum(),
-            pf[pf.tale].wordcount.sum()))
-        print(template_float.format(
             '    Average:',
-            pf.wordcount.mean(),
-            pf[pf.scp].wordcount.mean(),
-            pf[pf.tale].wordcount.mean()))
-        print(template_float.format(
             'Total Comments:',
-            pf.comments.sum(),
-            pf[pf.scp].comments.sum(),
-            pf[pf.tale].comments.sum()))
-        print(template_float.format(
-            '    Average:',
-            pf.comments.mean(),
-            pf[pf.scp].comments.mean(),
-            pf[pf.tale].comments.mean()))
-        print(template_float.format(
             'Total Revisions:',
-            pf.revisions.sum(),
-            pf[pf.scp].revisions.sum(),
-            pf[pf.tale].revisions.sum()))
-        print(template_float.format(
-            '    Average:',
-            pf.revisions.mean(),
-            pf[pf.scp].revisions.mean(),
-            pf[pf.tale].revisions.mean()))
-        print(template_float.format(
-            'Total Images:',
-            pf.images.sum(),
-            pf[pf.scp].images.sum(),
-            pf[pf.tale].images.sum()))
-        print('-' * 70)
+            'Total Images:')
+        funcs = (
+            lambda x: x.index.size,
+            lambda x: x.author.nunique(),
+            lambda x: self.votes[self.votes.page.isin(x.index)].index.size,
+            lambda x: self.votes[
+                (self.votes.page.isin(x.index)) &
+                (self.votes.value == 1)].index.size,
+            lambda x: self.votes[
+                (self.votes.page.isin(x.index)) &
+                (self.votes.value == -1)].index.size,
+            lambda x: x.rating.sum(),
+            lambda x: x.rating.mean(),
+            lambda x: x.rating.mode()[0],
+            lambda x: x.rating.std(),
+            lambda x: x.wordcount.sum(),
+            lambda x: x.wordcount.mean(),
+            lambda x: x.comments.sum(),
+            lambda x: x.history.sum(),
+            lambda x: x.images.sum(),
+        )
+        skips = self.tags[self.tags.tag == 'scp'].page
+        tales = self.tags[self.tags.tag == 'tale'].page
+        header = '| {:<33} | {:<8} | {:<8} | {:<8} |'
+        header = header.format('Field', 'Total', 'Skips', 'Tales')
+        print(header)
+        row = '| {:<33} | {:<8.0f} | {:<8.0f} | {:<8.0f} |'
+        for msg, func in zip(msgs, funcs):
+            print(row.format(
+                msg,
+                func(self.pages),
+                func(self.pages[self.pages.index.isin(skips)]),
+                func(self.pages[self.pages.index.isin(tales)])))
 
+    def create_table_authors(self):
+        pf = self.pages
+        rf = self.revisions
+        cf = self.comments
+        columns = {}
+        aus = pf.author.dropna().unique()
+        au = lambda x: pf[pf.author == x]
+        columns['pages created'] = {i: au(i).index.size for i in aus}
+        columns['net rating'] = {i: au(i).rating.sum() for i in aus}
+        columns['average rating'] = {i: au(i).rating.mean().round(2) for i in aus}
+        columns['rating per day'] = {}
+        for i in aus:
+            first_comment = cf[cf.user == i].time.dropna().min()
+            first_revision = rf[rf.user == i].time.dropna().min()
+            if pd.isnull(first_comment) and pd.isnull(first_revision):
+                continue
+            elif pd.isnull(first_comment):
+                first_activity = arrow.get(first_revision)
+            elif pd.isnull(first_revision):
+                first_activity = arrow.get(first_comment)
+            else:
+                first_activity = min(
+                    arrow.get(first_comment), arrow.get(first_revision))
+            days_on_site = (arrow.now() - first_activity).days
+            rating = au(i).rating.sum() / days_on_site
+            columns['rating per day'][i] = rating
+        columns['wordcount'] = {i: au(i).wordcount.sum() for i in aus}
+        columns['average wordcount'] = {i: au(i).wordcount.mean() for i in aus}
+        columns['image count'] = {i: au(i).images.sum() for i in aus}
+        table_authors = pd.DataFrame(columns)
+        table_authors.to_csv(self.dir + 'table_authors.csv', index_col='user')
 
 ###############################################################################
 
