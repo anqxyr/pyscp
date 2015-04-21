1c1ca1d5635c9538d2140bfc8f8380c6ee41323f
anqxyr <anqxyr@gmail.com>
2015-04-21 19:37:47 +0300

    flatten log_call decorator

--- pyscp/utils.py
+++ pyscp/utils.py
@@ -35,32 +35,32 @@ def morph_exc(catch_exc, raise_exc, message=None):
     return decorator
 
 
-def log_call(logger=print):
+class LogCallDecorator:
 
-    class LogCallDecorator:
+    def __init__(self, fn, logger):
+        self.fn = fn
+        self.logger = logger
+        self.ismethod = False
+        update_wrapper(self, fn)
 
-        def __init__(self, fn):
-            self.fn = fn
-            self.ismethod = False
-            update_wrapper(self, fn)
+    def __call__(self, *args, **kw):
+        argsrepr = chain(
+            map(repr, args),
+            ('{}={}'.format(k, repr(v)) for k, v in kw.items()))
+        if self.ismethod:
+            next(argsrepr)  # don't print self
+            message = '{}.{}'.format(
+                args[0].__class__.__name__, self.fn.__name__)
+        else:
+            message = '{}'.format(self.fn.__name__)
+        if argsrepr:
+            message += '({})'.format(', '.join(argsrepr))
+        self.logger(message)
+        return self.fn(*args, **kw)
 
-        def __call__(self, *args, **kw):
-            argsrepr = chain(
-                map(repr, args),
-                ('{}={}'.format(k, repr(v)) for k, v in kw.items()))
-            if self.ismethod:
-                next(argsrepr)  # don't print self
-                message = '{}.{}'.format(
-                    args[0].__class__.__name__, self.fn.__name__)
-            else:
-                message = '{}'.format(self.fn.__name__)
-            if argsrepr:
-                message += '({})'.format(', '.join(argsrepr))
-            logger(message)
-            return self.fn(*args, **kw)
+    def __get__(self, obj, objtype):
+        self.ismethod = True
+        return partial(self.__call__, obj)
 
-        def __get__(self, obj, objtype):
-            self.ismethod = True
-            return partial(self.__call__, obj)
 
-    return LogCallDecorator
+log_call = lambda logger=print: lambda fn: LogCallDecorator(fn, logger)
