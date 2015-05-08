c8abb8f2e53d15b87d0c2f73f6694bac0b0bbea7
anqxyr <anqxyr@gmail.com>
2015-05-08 10:12:18 +0300

    more gui things

--- pyscp/image_review.py
+++ pyscp/image_review.py
@@ -4,8 +4,21 @@
 # Module Imports
 ###############################################################################
 
+import pyscp
+import logging
+
+from bs4 import BeautifulSoup as bs4
 from PySide import QtGui, QtCore
 
+###############################################################################
+# Global Constants And Variables
+###############################################################################
+
+log = logging.getLogger('pyscp')
+req = pyscp.core.InsistentRequest()
+
+###############################################################################
+# GUI Wrappers
 ###############################################################################
 
 
@@ -15,80 +28,157 @@ class Widget:
         _args, widgets, self._children = [], [], []
         for i in args:
             (widgets if isinstance(i, Widget) else _args).append(i)
-        self._base_init(*_args, **kw)
+        self._init_obj(*_args, **kw)
         self.add(*widgets)
 
-    def __getattr__(self, name):
-        return getattr(self._base, name)
+    def _init_obj(self, *args, **kw):
+        layout = kw.pop('layout', 'vbox')
+        self._obj = QtGui.QWidget(*args, **kw)
+        self._obj.setLayout(dict(
+            vbox=QtGui.QVBoxLayout,
+            hbox=QtGui.QHBoxLayout,
+            grid=QtGui.QGridLayout,
+            form=QtGui.QFormLayout)[layout]())
 
-    def _base_init(self, *args, **kw):
-        self._base = QtGui.QWidget(*args, **kw)
-        self.setLayout(QtGui.QVBoxLayout())
-
-    def _base_add(self, base_widget):
-        self.layout().addWidget(base_widget)
+    def _add_child(self, widget):
+        self._obj.layout().addWidget(widget._obj)
 
     def add(self, *widgets):
         for widget in widgets:
-            widget.parent = widget
+            widget.parent = self
             self._children.append(widget)
-            self._base_add(widget._base)
+            self._add_child(widget)
+
+    def update_ui(self):
+        QtGui.qApp.processEvents()
 
 
-class App(Widget):
+class MainWindow(Widget):
 
-    def __init__(self, title):
+    def _init_obj(self, *args, **kw):
+        layout = kw.pop('layout', 'vbox')
+        self._obj = QtGui.QMainWindow(*args, **kw)
+        self._center = Widget(layout=layout)
+        self._obj.setCentralWidget(self._center._obj)
+
+    def _add_child(self, widget):
+        if isinstance(widget, MenuBar):
+            self._menubar = widget
+            self._obj.setMenuBar(widget._obj)
+        else:
+            self._center.add(widget)
+
+    def set_layout(self, layout):
+        self._center.set_layout(layout)
+
+
+class App(MainWindow):
+
+    def __init__(self, title, *, layout='vbox'):
         self._app = QtGui.QApplication('')
-        super().__init__()
-        self.setWindowTitle(title)
-        self.init_ui()
+        super().__init__(layout=layout)
+        self._obj.setWindowTitle(title)
 
     def run(self):
-        self.show()
+        self._obj.show()
         self._app.exec_()
-
-    def init_ui(self):
-        pass
 
 
 class Action(Widget):
 
-    def _base_init(self, name, fn):
-        self._base = QtGui.QAction(name, None)
-        self.triggered.connect(fn)
+    def _init_obj(self, name, fn, shortcut=None):
+        self._obj = QtGui.QAction(name, None)
+        if not shortcut and '&' in name:
+            shortcut = 'Ctrl+' + name[name.index('&') + 1]
+        self._obj.setShortcut(shortcut)
+        self._obj.triggered.connect(fn)
 
 
 class Menu(Widget):
 
-    def _base_init(self, name):
-        self._base = QtGui.QMenu(name)
+    def _init_obj(self, name):
+        self._obj = QtGui.QMenu(name)
 
-    def _base_add(self, base_widget):
-        self.addAction(base_widget)
+    def _add_child(self, widget):
+        self._obj.addAction(widget._obj)
 
 
 class MenuBar(Widget):
 
-    def _base_init(self):
-        self._base = QtGui.QMenuBar()
+    def _init_obj(self):
+        self._obj = QtGui.QMenuBar()
 
-    def _base_add(self, base_widget):
-        self.addMenu(base_widget)
+    def _add_child(self, widget):
+        self._obj.addMenu(widget._obj)
+
+
+class Button(Widget):
+
+    def _init_obj(self, name, fn):
+        self._obj = QtGui.QPushButton(name)
+        self._obj.clicked.connect(fn)
+
+
+class Label(Widget):
+
+    def _init_obj(self, text):
+        self._obj = QtGui.QLabel(text)
+
+
+class Picture(Widget):
+
+    def _init_obj(self, url):
+        image_data = req.get(url).content
+        pixmap = QtGui.QPixmap()
+        pixmap.loadFromData(image_data)
+        self._obj = QtGui.QLabel()
+        self._obj.setPixmap(pixmap)
 
 
 ###############################################################################
 
-def main():
-    app = App('SCP Image Review Helper')
-    app.add(
-        MenuBar(
-            Menu(
-                '&File',
-                Action('&Open', lambda: None),
-                Action('&Quit', app.close)
-            )
-        ))
-    app.run()
+class ImageBox(Widget):
+
+    def __init__(self, image, page, user, source, status, comments):
+        super().__init__(layout='hbox')
+        self.add(Picture(image))
+
+
+class ImageReview(App):
+
+    def __init__(self):
+        super().__init__('SCPIRT', layout='vbox')
+        self.sbox = pyscp.WikidotConnector('scpsandbox2')
+        self.add(
+            MenuBar(
+                Menu(
+                    '&File',
+                    Action('&Quit', self._obj.close)),
+                Menu(
+                    '&Images', *[
+                        Action(
+                            'View Block {}'.format(i),
+                            lambda i=i: self.view_block(i))
+                        for i in range(1, 21)])))
+
+    def view_block(self, index):
+        block_html = self.sbox('image-review-{}'.format(index)).html
+        tables = bs4(block_html)(class_='wiki-content-table')
+        for row in [i('td') for table in tables for i in table('tr')[1:]]:
+            user, source = None, None
+            if row[1]('span'):
+                user = row[1].span.a.text
+            if row[3]('a'):
+                source = row[3].a['href']
+            self.add(ImageBox(
+                image=row[0]('img')[0]['src'],
+                page=row[1].a['href'],
+                user=user,
+                source=source,
+                status=row[4].text,
+                comments=row[5] if row[5].text else None))
+            return
+
 
 if __name__ == '__main__':
-    main()
+    ImageReview().run()
