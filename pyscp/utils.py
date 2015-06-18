2b625b58131448b569ec8c8d047ca3374ce6d383
anqxyr <anqxyr@gmail.com>
2015-06-18 11:17:31 +0300

    remove funcy dependancy

--- pyscp/utils.py
+++ pyscp/utils.py
@@ -4,20 +4,55 @@
 # Module Imports
 ###############################################################################
 
-import funcy
 import logging
 import blessings
 import re
 import time
 import threading
 import signal
+import functools
+import inspect
 
 ###############################################################################
 # Decorators
 ###############################################################################
 
+###############################################################################
+# Decorator decorator is a simplified version of the code from the funcy lib.
+# https://github.com/Suor/funcy
+###############################################################################
 
-decorator = funcy.decorator
+
+class Call:
+
+    def __init__(self, func, args, kwargs):
+        self.func, self.args, self.kwargs = func, args, kwargs
+
+    def __call__(self):
+        return self.func(*self.args, **self.kwargs)
+
+
+def decorator(deco):
+    spec = inspect.getargspec(deco)
+    if len(spec.args) > 1 or spec.varargs or spec.keywords:
+        @functools.wraps(deco)
+        def _fab(*dargs, **dkwargs):
+            return make_decorator(deco, *dargs, **dkwargs)
+        return _fab
+    else:
+        return functools.wraps(deco)(make_decorator(deco))
+
+
+def make_decorator(deco, *dargs, **dkwargs):
+    def _decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            call = Call(func, args, kwargs)
+            return deco(call, *dargs, **dkwargs)
+        return wrapper
+    return _decorator
+
+###############################################################################
 
 
 @decorator
@@ -58,10 +93,10 @@ def log_calls(call, logger=print):
 
 @decorator
 def decochain(call, *decs):
-    fn = call._func
+    fn = call.func
     for dec in reversed(decs):
         fn = dec(fn)
-    return fn(*call._args, **call._kwargs)
+    return fn(*call.args, **call.kwargs)
 
 ###############################################################################
 
