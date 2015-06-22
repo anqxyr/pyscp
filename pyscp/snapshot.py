afcddab8cb47c634afb72f39ad834cd3c19b6329
anqxyr <anqxyr@gmail.com>
2015-06-22 13:43:37 +0300

    finish moving code to the snapshot submodule

--- pyscp/snapshot.py
+++ pyscp/snapshot.py
@@ -58,106 +58,35 @@ class Wiki(pyscp.core.Wiki):
     # Internal Methods
     ###########################################################################
 
-    def _list_author(self, author):
-        query = (
-            orm.Page.select(orm.Page.url)
-            .join(orm.Revision).join(orm.User)
-            .where(orm.Revision.number == 0)
-            .where(orm.User.name == author))
-        include, exclude = [], []
-        for over in self.list_overrides():
-            if over.user == author:
-                include.append(over.url)
-            elif over.type == 'author':
-                exclude.append(over.url)
-        return (query.where(~(orm.Page.url << exclude)) |
-                orm.Page.select(orm.Page.url).where(orm.Page.url << include))
-
-    def _list_rating(self, rating):
-        op, value, *_ = re.split('([0-9]+)', rating)
-        if op not in ('>', '<', '>=', '<=', '=', ''):
-            raise ValueError
-        if not op or op == '=':
-            op = '=='
-        compare = lambda x: eval('x {} {}'.format(op, value))
-        return (orm.Page.select(orm.Page.url)
-                .join(orm.Vote).group_by(orm.Page.url)
-                .having(compare(orm.peewee.fn.sum(orm.Vote.value))))
-
-    def _list_created(self, created):
-        op, date, *_ = re.split('([0-9-]+)', created)
-        year, month, day, *_ = date.split('-') + [None, None]
-        if op not in ('>', '<', '>=', '<=', '=', ''):
-            raise ValueError
-        code_year = '(x.year {{}} {})'.format(year)
-        if op and op != '=':
-            if op in ('<', '>='):
-                month = month if month else 1
-                day = day if day else 1
-            else:
-                month = month if month else 12
-                day = day if day else 31
-            code_month = '(x.month {{}} {})'.format(month)
-            code_day = '(x.day {{}} {})'.format(day)
-            code_string = '({0} | ({0} & {1}) | ({0} & {1} & {2}))'.format(
-                code_year, code_month, code_day)
-            code_string = code_string.format(op, '==', op, '==', '==', op)
-        else:
-            code_string = code_year.format('==')
-            if month:
-                code_string += ' & (x.month == {})'.format(int(month))
-            if day:
-                code_string += ' & (x.day == {})'.format(int(day))
-            code_string = '({})'.format(code_string)
-        compare = lambda x: eval(code_string)
-        return (orm.Page.select(orm.Page.url)
-                .join(orm.Revision).where(orm.Revision.number == 0)
-                .group_by(orm.Page.url)
-                .having(compare(orm.Revision.time)))
-
-    ###########################################################################
-    # Public Methods
-    ###########################################################################
-
-    def list_pages(self, **kwargs):
-        query = orm.Page.select(orm.Page.url)
-        if 'author' in kwargs:
-            query = query & self._list_author(kwargs['author'])
+    def _urls(self, **kwargs):
+        page = pyscp.orm.Page
+        ptag = pyscp.orm.PageTag
+        tag = pyscp.orm.Tag
+        query = page.select(page.url)
         if 'tag' in kwargs:
-            query = query & (
-                orm.Page.select(orm.Page.url)
-                .join(orm.PageTag).join(orm.Tag)
-                .where(orm.Tag.name == kwargs['tag']))
-        if 'rating' in kwargs:
-            query = query & self._list_rating(kwargs['rating'])
-        if 'created' in kwargs:
-            query = query & self._list_created(kwargs['created'])
-        if 'order' in kwargs:
-            query = query.order_by(orm.peewee.fn.Random())
-        else:
-            query = query.order_by(orm.Page.url)
+            query = query.join(ptag).join(tag).where(tag.name == kwargs['tag'])
         if 'limit' in kwargs:
             query = query.limit(kwargs['limit'])
-        for i in query:
-            yield i.url
+        for p in query:
+            yield p.url
 
     ###########################################################################
     # SCP-Wiki Specific Methods
     ###########################################################################
 
-    @functools.lru_cache()
-    @pyscp.utils.listify()
+    @functools.lru_cache(maxsize=1)
     def list_overrides(self):
-        for row in (
-                orm.Override
-                .select(orm.Override, orm.User.name, orm.OverrideType.name)
-                .join(orm.User).switch(orm.Override).join(orm.OverrideType)):
-            yield Override(row._data['url'], row.user.name, row.type.name)
+        ov = pyscp.orm.Override
+        ot = pyscp.orm.OverrideType
+        us = pyscp.orm.User
+        query = ov.select(ov, us.name, ot).join(us).switch(ov).join(ot)
+        return [pyscp.core.Override(r._data['url'], r.user.name, r.type.name)
+                for r in query]
 
     @functools.lru_cache()
     @pyscp.utils.listify()
     def images(self):
-        for row in orm.Image.select():
+        for row in pyscp.orm.Image.select():
             yield dict(
                 url=row.url, source=row.source, status=row.status.label,
                 notes=row.notes, data=row.data)
@@ -176,12 +105,10 @@ class Page(pyscp.core.Page):
     # Internal Methods
     ###########################################################################
 
-    def _query(self, ptable, stable='User', key=None):
+    def _query(self, ptable, stable='User'):
         """Generate SQL queries used to retrieve data."""
-        if key is None:
-            key = lambda x: x.page == self._id
         pt, st = [getattr(pyscp.orm, i) for i in (ptable, stable)]
-        return pt.select(pt, st.name).join(st).where(key(pt)).execute()
+        return pt.select(pt, st.name).join(st).where(pt.page == self._id)
 
     @pyscp.utils.cached_property
     def _pdata(self):
@@ -216,9 +143,29 @@ class Page(pyscp.core.Page):
                 r.id, r.number, r.user.name, str(r.time), r.comment)
                 for r in revs]
 
+    @pyscp.utils.cached_property
+    def votes(self):
+        """Return all votes made on the page."""
+        return [pyscp.core.Vote(v.user.name, v.value)
+                for v in self._query('Vote')]
+
+    @pyscp.utils.cached_property
+    def tags(self):
+        """Return the set of tags with which the page is tagged."""
+        return {pt.tag.name for pt in self._query('PageTag', 'Tag')}
+
 
 class Thread(pyscp.core.Thread):
-    pass
+
+    @pyscp.utils.cached_property
+    def posts(self):
+        fp = pyscp.orm.ForumPost
+        us = pyscp.orm.User
+        query = fp.select(fp, us.name).join(us).where(fp.thread == self._id)
+        return [pyscp.core.Post(
+                p.id, p.title, p.content, p.user.name,
+                str(p.time), p._data['parent'])
+                for p in query]
 
 
 Wiki.Page = Page
