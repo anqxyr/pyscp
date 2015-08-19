5d11f016b1ea27a5e5c9aec3b3e9be5870bc8bde
anqxyr <anqxyr@gmail.com>
2015-08-19 18:00:09 +0300

    move scpirt into a separate repo

--- scpirt/ui/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-from .mainwindow import Ui_MainWindow as MainWindow 
-from .imagebox import Ui_imagebox as ImageBox
-from .login import Ui_LoginForm as LoginForm
--- scpirt/ui/update.sh
+++
@@ -1,1 +0,0 @@
-for i in *.ui; do pyside-uic $i -o ${i::-3}.py; done
